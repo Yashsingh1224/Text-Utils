2594f1b50cc161a01903c3611bf5c3fc03eeea3a
Yash singh <yk73690932@gmail.com>
2025-01-03 18:46:05 +0530

    second commit

---
+++ build/precache-manifest.7cb48a13c8551f012ec2d13f0ef441d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfcdd9af1e9941e0cc897b716e504ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83119470078fad51d76c",
+    "url": "/static/css/main.5a08396e.chunk.css"
+  },
+  {
+    "revision": "9cebd88478a5e6d7af3a",
+    "url": "/static/js/2.4494f7de.chunk.js"
+  },
+  {
+    "revision": "91b4d3c7233d5914416d",
+    "url": "/static/js/3.d0f07a8d.chunk.js"
+  },
+  {
+    "revision": "83119470078fad51d76c",
+    "url": "/static/js/main.47788601.chunk.js"
+  },
+  {
+    "revision": "5d0274418201353e58cd",
+    "url": "/static/js/runtime~main.4ec212c0.js"
+  }
+]);
